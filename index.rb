24aafe56d930b68881e40f4975f331e0abdd3a3f
JC-Ferguson <jcferguson1996@gmail.com>
2019-09-03 11:42:51 +1000

    added ladder file to house feature of the app, slightalterations to teams and index to clean code

---
+++ index.rb
@@ -0,0 +1,4 @@
+require_relative "./teams"
+
+puts "Hi, welcome to the Rugby League App\nPlease choose what you would like to do.  \n(Ladder | Draw | Select Team)"
+    option=gets.chomp.downcase
--- teams.rb
+++ teams.rb
@@ -4,7 +4,7 @@ class Teams
     #             :interchange_1, :interchange_2, :interchange_3, :interchange_4
 
     def initialize(fullback, left_wing, right_wing, left_centre, right_centre, halfback, five_eighth,
-                   left_prop, right_prop, hooker, left_second_row, right_second_row, lock, 
+                   left_prop, hooker, right_prop, left_second_row, right_second_row, lock, 
                    interchange_1, interchange_2, interchange_3, interchange_4)
 
             @fullback=fullback
@@ -28,29 +28,28 @@ class Teams
     end
 
     def team_display
-        p @fullback
-        p @left_wing
-        p @left_centre
-        p @right_centre
-        p @right_wing
-        p @five_eighth
-        p @halfback
-        p @left_prop
-        p @hooker
-        p @right_prop
-        p @left_second_row
-        p @right_second_row
-        p @lock
-        p @interchange_1
-        p @interchange_2
-        p @interchange_3
-        return @interchange_4
+        puts @fullback
+        puts @left_wing
+        puts @left_centre
+        puts @right_centre
+        puts @right_wing
+        puts @five_eighth
+        puts @halfback
+        puts @left_prop
+        puts @hooker
+        puts @right_prop
+        puts @left_second_row
+        puts @right_second_row
+        puts @lock
+        puts @interchange_1
+        puts @interchange_2
+        puts @interchange_3
+        puts @interchange_4
     end
    
 end
 
-    cronulla_sharks= Teams.new( "1. Josh Dugan","2. Sione Katoa","3. Bronson Xerri","4. Josh Morris","5. Ronaldo Mulitalo","6. Shaun Johnson",
-                                "7. Chad Townsend","8. Aaron Woods","9. Jayden Brailey","10. Matt Prior","11. Briton Nikora","12. Wade Graham",
-                                "13. Paul Gallen (c)","14. Kurt Capewell","15. Braden Hamlin-Uele","16. Andrew Fifita", "17. Jayson Bukuya")
+cronulla_sharks= Teams.new( "1. Josh Dugan","2. Sione Katoa","3. Bronson Xerri","4. Josh Morris","5. Ronaldo Mulitalo","6. Shaun Johnson",
+                            "7. Chad Townsend","8. Aaron Woods","9. Jayden Brailey","10. Matt Prior","11. Briton Nikora","12. Wade Graham",
+                            "13. Paul Gallen (c)","14. Kurt Capewell","15. Braden Hamlin-Uele","16. Andrew Fifita", "17. Jayson Bukuya")
                     
-p cronulla_sharks.team_display
